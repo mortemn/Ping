20093fe4ab2d09c05446afd03defc9b4470621ff
p39663ma <marcus.aviles@student.manchester.ac.uk>
2024-02-18 14:24:07 +0000

    semi working authentication, bare bones

--- authentication/main.go
+++ authentication/main.go
@@ -1,76 +1,91 @@
 package main
 
-import "fmt"
 import (
 	"log"
-	"encoding/json"
-	"github.com/golang-jwt/jwt/v5"
 	"net/http"
+	"encoding/json"
+	"fmt"
 	"time"
-
+	"github.com/golang-jwt/jwt/v5"
 )
 
-
-func main(){
-	http.HandleFunc("/singin", Signin)
+func main() {
+	// "Signin" and "Welcome" are the handlers that we will implement
+	http.HandleFunc("/signin", Signin)
 	http.HandleFunc("/welcome", Welcome)
 	http.HandleFunc("/refresh", Refresh)
 	http.HandleFunc("/logout", Logout)
 
+	// start the server on port 8000
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-
 var jwtKey = []byte("my_secret_key")
+
 var users = map[string]string{
-	"user1":"password1",
-	"user2":"password2",
+	"user1": "password1",
+	"user2": "password2",
 }
 
+// Create a struct that models the structure of a user, both in the request body, and in the DB
 type Credentials struct {
 	Password string `json:"password"`
 	Username string `json:"username"`
 }
 
-type Claims struct{
-	Username string `json:"Username"`
+type Claims struct {
+	Username string `json:"username"`
 	jwt.RegisteredClaims
 }
 
-func Signin(w http.ResponseWriter, r *http.Request){
+func Signin(w http.ResponseWriter, r *http.Request) {
 	var creds Credentials
-	err:= json.NewDecoder(r.Body).Decode(&creds)
-	if err != nil{
+	// Get the JSON body and decode into credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		// If the structure of the body is wrong, return an HTTP error
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	// Get the expected password from our in memory map
 	expectedPassword, ok := users[creds.Username]
 
-	if !ok || expectedPassword != creds.Password{
+	// If a password exists for the given user
+	// AND, if it is the same as the password we received, the we can move ahead
+	// if NOT, then we return an "Unauthorized" status
+	if !ok || expectedPassword != creds.Password {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
+	// Declare the expiration time of the token
+	// here, we have kept it as 5 minutes
 	expirationTime := time.Now().Add(5 * time.Minute)
-	
-	claims:= &Claims{
-		Username:creds.Username,
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Username: creds.Username,
 		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
 			ExpiresAt: jwt.NewNumericDate(expirationTime),
 		},
 	}
 
+	// Declare the token with the algorithm used for signing, and the claims
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
+	// Create the JWT string
 	tokenString, err := token.SignedString(jwtKey)
-	if err != nil{
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
 	http.SetCookie(w, &http.Cookie{
-		Name: "token",
-		Value: tokenString, 
+		Name:    "token",
+		Value:   tokenString,
 		Expires: expirationTime,
 	})
 }
@@ -119,9 +134,8 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
 }
 
-
 func Refresh(w http.ResponseWriter, r *http.Request) {
-	// (BEGIN) The code until this point is the same as the first part of the `Welcome` route
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
 	c, err := r.Cookie("token")
 	if err != nil {
 		if err == http.ErrNoCookie {
@@ -148,7 +162,7 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-	// (END) The code until this point is the same as the first part of the `Welcome` route
+	// (END) The code uptil this point is the same as the first part of the `Welcome` route
 
 	// We ensure that a new token is not issued until enough time has elapsed
 	// In this case, a new token will only be issued if the old token is within
@@ -175,7 +189,6 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 		Expires: expirationTime,
 	})
 }
-
 
 func Logout(w http.ResponseWriter, r *http.Request) {
 	// immediately clear the token cookie
