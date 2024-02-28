7a88f7282031c59378052065fcc337db7ea78300
y77454je <joshua.edgar-3@student.manchester.ac.uk>
2024-02-28 16:41:38 +0000

    Added new page which can display the map using a component which takes the width and height to display as parameters

---
+++ frontend/src/app/DisplayMap/page.js
@@ -0,0 +1,9 @@
+import { Map } from "@/components/map";
+
+export default function Home() {
+  return (
+    <main>
+      <Map w={1920} h={400} />
+    </main>
+  );
+}
---
+++ frontend/src/components/map.jsx
@@ -0,0 +1,65 @@
+"use client"
+
+import React, { useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+
+export function Map({w, h}) {
+  
+    const mapRef = React.useRef(null);
+
+    useEffect(() => {
+
+        const initMap = async () => {
+            
+            
+            const loader = new Loader({
+                apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
+                version: "weekly"
+            });
+            
+            const { Map } = await loader.importLibrary('maps')
+
+            //initialise marker
+            const { Marker } = await loader.importLibrary('marker');
+
+            const position = {
+                lat: 5,
+                lng: -79
+            }
+
+            //map Options
+            const mapOptions = {
+                center: position,
+                zoom: 17,
+                mapId: 'MY_NEXTJS_MAPID'
+            }
+
+            // Map setup
+
+            const map = new Map(mapRef.current, mapOptions);
+
+            //Place marker
+            const marker = new Marker({
+                position: position,
+                map: map
+            });
+
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                  const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                  };
+                  map.setCenter(pos);
+                  marker.setPosition(pos);
+                }
+            );
+        }
+
+        initMap();
+    }, []);
+
+    return (
+        <div style={{ height: h, width: w}} ref={mapRef} />
+    )
+}
