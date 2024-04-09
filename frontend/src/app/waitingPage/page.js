57be8fcbbe5fbcd735c1ea21819d8af8499d77dd
j25224pl <pei.lee-3@student.manchester.ac.uk>
2024-04-09 18:48:08 +0100

    Title with animation

--- frontend/src/app/waitingPage/page.js
+++ frontend/src/app/waitingPage/page.js
@@ -5,14 +5,25 @@ export default function Home() {
     return ( 	
         <body>
             <header>
-                <h2 className={styles.logo}><span>P</span><span style={{color: 'red'}}>I</span><span>N</span><span>G</span></h2>
+                <h2 className={styles.logo}>
+                    <span>P</span>
+                    <span style={{color: 'red'}}>I</span>
+                    <span>N</span>
+                    <span>G</span>
+                </h2>
             </header>
+            {/* <ion-icon
+                name="location"
+                className={styles["location-icon"]}
+            ></ion-icon> */}
 
             <form className={styles.form}>
                 <span className= {styles.title}>Players Joining, Please Wait.</span>
                 <span className= {styles.title}>Game Starting Soon...</span>
                 <button className= {styles.btn} type="submit" disabled>display number of players joined</button>
             </form>
+            {/* <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+            <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> */}
         </body>
     );
 }
