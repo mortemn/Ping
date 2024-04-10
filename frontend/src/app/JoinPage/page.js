07cc457e6300f700296c0cc5ff8858e1da9bb7e7
y77454je <joshua.edgar-3@student.manchester.ac.uk>
2024-04-10 18:35:36 +0100

    Updated home page

--- frontend/src/app/JoinPage/page.js
+++ frontend/src/app/JoinPage/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 
+
+
 export default function Home() {
     return ( 	
         <body>
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,95 +1,75 @@
-import Image from "next/image";
+import Head from "next/head";
 import styles from "./page.module.css";
+import Link from "next/link";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <body>
+      <header className={styles["header"]}>
+        <h2 className={styles["logo"]}>
+          P<span className={styles["red-letter"]}>I</span>NG
+        </h2>
+        <nav className={styles["navigation"]}>
+          <Link href="/loginPage">
+            <button className={styles["btn"]}>LOG IN</button>
+          </Link>
+          <Link href = "">
+            <button className={styles["btn"]}>ACCOUNT</button>
+          </Link>
+        </nav>
+      </header>
+      <div className={styles.container}>
+        <div className={styles["top-container"]}>
+          <div className={styles["left-panel"]}>
+            <p>
+              P<span className={styles["red-letter-2"]}>I</span>NG, a large area
+              game
+              <br />
+              of hide and seek in the
+              <br />
+              University of Manchester
+            </p>
+            <div className={styles["how-to-pair"]}>
+              <ion-icon
+                name="location"
+                className={styles["location-icon"]}
+              ></ion-icon>
+              <div className={styles["how-to-play"]}>HOW TO PLAY PING</div>
+            </div>
+            <div className={styles["instructions"]}>
+              1. Host or Join a room with your friends.<br/>
+              2. Hiders will hide while seekers will seek.<br/>
+              3. When hiders are caught, they will be switched over to the<br/> seekers team and continue the game as seekers.<br/>
+              4. The game ends when the time is up or all hiders have been caught.<br/>
+              5. Hide longer to accumulate more scores;<br/> Catch more hiders to accumulate more scores.<br/>
+              6. Have fun! <br/>Remember: you can run but you can't hide...<br/>
+            </div>
+          </div>
+          <div className={styles["right-panel"]}>
+            <Link href="/roomPage">
+              <button className={styles["start-game-btn"]}>START GAME</button>
+            </Link>
+          </div>
+        </div>
+        <div className={styles["bottom-container"]}>
+          <div className={styles["leaderboards-title"]}>LEADERBOARDS</div>
+          <div className={styles["leaderboards-body"]}>
+            <div>TOP SEEKRS</div>
+            <div>TOTAL WINS</div>
+            <div>TOP HIDERS</div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        nomodule
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+      ></script>
+    </body>
   );
-}
+};
+
+export default HomePage;
