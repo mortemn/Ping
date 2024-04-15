693f9366e41ea7e760c68bffc7172cda62932aca
b03663pl <pak.lam-2@student.manchester.ac.uk>
2024-04-15 19:47:26 +0100

    Reverting back nvm

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,19 +1,24 @@
+import Head from "next/head";
 import styles from "./page.module.css";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <body>
-      <header>
+      <header className={styles["header"]}>
         <h2 className={styles["logo"]}>
           P<span className={styles["red-letter"]}>I</span>NG
         </h2>
         <nav className={styles["navigation"]}>
-          <button className={styles["btn"]}>LOG OUT</button>
-          <button className={styles["btn"]}>ACCOUNT</button>
+          <Link href="/loginPage">
+            <button className={styles["btn"]}>LOG IN</button>
+          </Link>
+          <Link href = "/profilePage">
+            <button className={styles["btn"]}>ACCOUNT</button>
+          </Link>
         </nav>
       </header>
-      <main>
+      <div className={styles.container}>
         <div className={styles["top-container"]}>
           <div className={styles["left-panel"]}>
             <p>
@@ -22,20 +27,26 @@ const HomePage = () => {
               <br />
               of hide and seek in the
               <br />
-              University of <br />
-              Manchester
+              University of Manchester
             </p>
             <div className={styles["how-to-pair"]}>
               <ion-icon
                 name="location"
                 className={styles["location-icon"]}
-                
               ></ion-icon>
               <div className={styles["how-to-play"]}>HOW TO PLAY PING</div>
             </div>
+            <div className={styles["instructions"]}>
+              1. Host or Join a room with your friends.<br/>
+              2. Hiders will hide while seekers will seek.<br/>
+              3. When hiders are caught, they will be switched over to the<br/> seekers team and continue the game as seekers.<br/>
+              4. The game ends when the time is up or all hiders have been caught.<br/>
+              5. Hide longer to accumulate more scores;<br/> Catch more hiders to accumulate more scores.<br/>
+              6. Have fun! <br/>Remember: you can run but you can&apos;t hide...<br/>
+            </div>
           </div>
           <div className={styles["right-panel"]}>
-            <Link href="/roomPage1">
+            <Link href="/RoomPage">
               <button className={styles["start-game-btn"]}>START GAME</button>
             </Link>
           </div>
@@ -43,21 +54,12 @@ const HomePage = () => {
         <div className={styles["bottom-container"]}>
           <div className={styles["leaderboards-title"]}>LEADERBOARDS</div>
           <div className={styles["leaderboards-body"]}>
-            <div>
-              TOP SEEKERS
-            </div>
-            <div>
-              TOTAL WINS
-            </div>
-            <div>
-              TOP HIDERS
-            </div>
+            <div>TOP SEEKRS</div>
+            <div>TOTAL WINS</div>
+            <div>TOP HIDERS</div>
           </div>
         </div>
-      </main>
-      <footer>
-        <button className={styles["btn"]}>button</button>
-      </footer>
+      </div>
       <script
         type="module"
         src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
@@ -71,3 +73,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
